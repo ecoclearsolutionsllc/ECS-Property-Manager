7d48576ace4b388dccc105d15fad76e5695a77b1
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2026-07-06 07:05:46 +0530

    chore: update service worker version and rebuild flutter web assets

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "860174528" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "795689717" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
